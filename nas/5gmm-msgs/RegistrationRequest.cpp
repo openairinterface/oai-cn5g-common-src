0f3dcaf2209f1309057dd95f4741155c564aa2b4
Tien Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>
2024-05-24 13:54:52 +0200

    Fix AMF ID for GUTI

--- nas/5gmm-msgs/RegistrationRequest.cpp
+++ nas/5gmm-msgs/RegistrationRequest.cpp
@@ -178,12 +178,12 @@ std::string RegistrationRequest::Get5gGuti() const {
   std::optional<_5G_GUTI_t> guti = std::nullopt;
   ie_5gs_mobile_identity_.Get5gGuti(guti);
   if (!guti.has_value()) return {};
-
+  std::string amf_id_str = {};
+  oai::utils::conv::get_amf_id(
+      guti.value().amf_region_id, guti.value().amf_set_id,
+      guti.value().amf_pointer, amf_id_str);
   std::string guti_str =
-      guti.value().mcc + guti.value().mnc +
-      std::to_string(guti.value().amf_region_id) +
-      std::to_string(guti.value().amf_set_id) +
-      std::to_string(guti.value().amf_pointer) +
+      guti.value().mcc + guti.value().mnc + amf_id_str +
       oai::utils::conv::tmsi_to_string(guti.value()._5g_tmsi);
   oai::logger::logger_registry::get_logger(LOGGER_COMMON)
       .debug("5G GUTI %s", guti_str.c_str());

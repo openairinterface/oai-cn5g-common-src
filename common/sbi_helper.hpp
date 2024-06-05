857d9a88873d2774675d9da1285cf616d0650b58
Tien Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>
2024-06-05 09:31:59 +0200

    Update LMF's endpoints

--- common/sbi_helper.hpp
+++ common/sbi_helper.hpp
@@ -642,13 +642,13 @@ class sbi_helper {
   // LMF: LMF Notify
   static inline const std::string LmfN2InfoNotifyBase = "/nlmf-n2info-notify/";
   static inline const std::string LmfN2InfoNotifyNrppaCallback =
-      "/nrppa/callback/";
-  static inline const std::string LmfN2InfoNotifyNrppaCallbackSupi =
-      "/nrppa/callback/:supi";
+      "/nrppa/callback";
+  static inline const std::string LmfN2InfoNotifyNrppaCallbackUeContextId =
+      "/nrppa/callback/:ueContextId";
   static inline const std::string LmfNonUeN2InfoNotifyBase =
       "/nlmf-non-ue-n2info-notify/";
   static inline const std::string LmfNonUeN2InfoNotifyNrppaCallback =
-      "/nrppa/callback/";
+      "/nrppa/callback";
 
   /*
    * Get NRF Nfm API Root

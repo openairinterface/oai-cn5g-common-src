96ee5befa859dcdc17572149b2d652a96e02af01
Tien Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>
2025-01-17 13:26:20 +0100

    Fix GPRS Timer IE

--- nas/ies/GprsTimer.cpp
+++ nas/ies/GprsTimer.cpp
@@ -27,7 +27,9 @@ using namespace oai::nas;
 GprsTimer::GprsTimer(uint8_t iei) : Type3NasIe(iei), value_() {}
 
 //------------------------------------------------------------------------------
-GprsTimer::GprsTimer(uint8_t iei, uint8_t value) : Type3NasIe(iei) {
+GprsTimer::GprsTimer(uint8_t iei, uint8_t unit, uint8_t value)
+    : Type3NasIe(iei) {
+  unit_  = unit;
   value_ = value;
 }
 
@@ -40,13 +42,26 @@ uint32_t GprsTimer::GetIeLength() const {
 }
 
 //------------------------------------------------------------------------------
-void GprsTimer::Set(uint8_t iei, uint8_t value) {
+void GprsTimer::Set(uint8_t iei, uint8_t unit, uint8_t value) {
   SetIei(iei);
+  unit_  = unit;
   value_ = value;
 }
 
 //------------------------------------------------------------------------------
+void GprsTimer::SetUnit(uint8_t unit) {
+  if (unit > 0x07) return;  // 3 bits
+  unit_ = unit;
+}
+
+//------------------------------------------------------------------------------
+uint8_t GprsTimer::GetUnit() const {
+  return unit_;
+}
+
+//------------------------------------------------------------------------------
 void GprsTimer::SetValue(uint8_t value) {
+  if (value > 0x1f) return;  // 5 bits
   value_ = value;
 }
 
@@ -70,8 +85,9 @@ int GprsTimer::Encode(uint8_t* buf, int len) const {
   int encoded_size = 0;
   // IEI
   encoded_size += Type3NasIe::Encode(buf + encoded_size, len);
-  // Value
-  ENCODE_U8(buf + encoded_size, value_, encoded_size);
+  // Unit+Value
+  uint8_t octet_2 = ((unit_ & 0x07) << 5) | (value_ & 0x1f);
+  ENCODE_U8(buf + encoded_size, octet_2, encoded_size);
 
   oai::logger::logger_common::nas().debug(
       "Encoded %s, len (%d)", GetIeName().c_str(), encoded_size);
@@ -91,12 +107,14 @@ int GprsTimer::Decode(const uint8_t* const buf, int len, bool is_iei) {
   uint8_t decoded_size = 0;
   // IEI
   decoded_size += Type3NasIe::Decode(buf + decoded_size, len, is_iei);
-
-  // Value
-  DECODE_U8(buf + decoded_size, value_, decoded_size);
-
+  // Unit+Value
+  uint8_t octet_2 = 0;
+  DECODE_U8(buf + decoded_size, octet_2, decoded_size);
+  unit_  = (octet_2 >> 5) & 0x07;
+  value_ = octet_2 & 0x1f;
   oai::logger::logger_common::nas().debug(
-      "Decoded %s, Value 0x%x", GetIeName().c_str(), value_);
+      "Decoded %s, Unit 0x%x, Value 0x%x", GetIeName().c_str(), unit_, value_);
+
   oai::logger::logger_common::nas().debug(
       "Decoded %s, len (%d)", GetIeName().c_str(), decoded_size);
   return decoded_size;
--- nas/ies/GprsTimer.hpp
+++ nas/ies/GprsTimer.hpp
@@ -30,11 +30,17 @@ constexpr uint8_t kGprsTimerContentLength =
 constexpr auto kGprsTimerIeName = "GPRS Timer";
 
 namespace oai::nas {
+constexpr uint8_t kGprsTimerUnitValueIsIncrementedInMultiplesOf2Seconds = 0b000;
+constexpr uint8_t kGprsTimerUnitValueIsIncrementedInMultiplesOf1Minute  = 0b001;
+constexpr uint8_t kGprsTimerUnitValueIsIncrementedInMultiplesOfDecihours =
+    0b010;
+constexpr uint8_t kGprsTimerUnitValueIndicatesThatTheTimerIsDeactivated = 0b111;
 
 class GprsTimer : public Type3NasIe {
  public:
   GprsTimer(uint8_t iei);
-  GprsTimer(uint8_t iei, uint8_t value);
+  GprsTimer(uint8_t iei, uint8_t unit, uint8_t value);
+
   virtual ~GprsTimer();
 
   int Encode(uint8_t* buf, int len) const override;
@@ -43,13 +49,17 @@ class GprsTimer : public Type3NasIe {
   static std::string GetIeName() { return kGprsTimerIeName; }
   uint32_t GetIeLength() const override;
 
-  void Set(uint8_t _iei, uint8_t value);
+  void Set(uint8_t _iei, uint8_t unit, uint8_t value);
+
+  void SetUnit(uint8_t unit);
+  uint8_t GetUnit() const;
 
   void SetValue(uint8_t value);
   uint8_t GetValue() const;
 
  private:
-  uint8_t value_;
+  uint8_t unit_;   // 3 bits
+  uint8_t value_;  // 5 bits
 };
 }  // namespace oai::nas
 

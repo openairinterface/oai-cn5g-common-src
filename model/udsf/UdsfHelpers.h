b58dd2430fc64b92381eb6ed53720cfeae0b06ee
Shubhika.Garg <Shubhika.Garg@OpenAirinterface.org>
2026-03-20 09:42:28 +0100

    chore(license): Update OAI License to Collaborative Standards Software License v1.0 (CSSL)
              - Also, remove the authors from the files

--- model/udsf/UdsfHelpers.h
+++ model/udsf/UdsfHelpers.h
@@ -1,3 +1,6 @@
+/*
+ * SPDX-License-Identifier: LicenseRef-CSSL-1.0
+ */
 #ifndef Udsf_Helpers_H_
 #define Udsf_Helpers_H_
 
--- ngap/libngap/aper_encoder.c
+++ ngap/libngap/aper_encoder.c
@@ -1,3 +1,6 @@
+/*
+ * SPDX-License-Identifier: LicenseRef-CSSL-1.0
+ */
 #include <asn_application.h>
 #include <asn_internal.h>
 #include <aper_encoder.h>

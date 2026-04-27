8224d9fe692eb95c2b7a2c77474eed4f3a8336c8
Franck Messaoudi <franck.messaoudi@eurecom.fr>
2026-04-27 13:18:27 +0200

    Remove: remove the commented deprecated code
    Signed-off-by: Franck Messaoudi <franck.messaoudi@eurecom.fr>

--- utils/string.cpp
+++ utils/string.cpp
@@ -13,30 +13,6 @@
 
 #include "logger_base.hpp"
 
-// Licence : https://creativecommons.org/licenses/by-sa/4.0/legalcode
-// https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring#217605
-
-// trim from start
-/* DEPRECATED CODE*/
-// std::string& oai::utils::ltrim(std::string& s) {
-//   s.erase(
-//       s.begin(),
-//       std::find_if(
-//           s.begin(), s.end(), std::not1(std::ptr_fun<int,
-//           int>(std::isspace))));
-//   return s;
-// }
-
-// // trim from end
-// std::string& oai::utils::rtrim(std::string& s) {
-//   s.erase(
-//       std::find_if(
-//           s.rbegin(), s.rend(), std::not1(std::ptr_fun<int,
-//           int>(std::isspace))) .base(),
-//       s.end());
-//   return s;
-// }
-
 std::string& oai::utils::ltrim(std::string& s) {
   s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
             return !std::isspace(ch);
